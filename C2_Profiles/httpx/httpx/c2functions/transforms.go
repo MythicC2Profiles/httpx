ce36c58ba08c929a258fdf26cfc7daa467b8e91b
Steve Embling <34773749+steve-embling@users.noreply.github.com>
2026-04-13 13:46:12 +0100

    testing base64url unpadded decode

--- C2_Profiles/httpx/httpx/c2functions/transforms.go
+++ C2_Profiles/httpx/httpx/c2functions/transforms.go
@@ -23,14 +23,21 @@ func transformBase64Reverse(prev []byte, value string) ([]byte, error) {
 func transformBase64URL(prev []byte, value string) ([]byte, error) {
 	return []byte(base64.URLEncoding.EncodeToString(prev)), nil
 }
+}
 func transformBase64URLReverse(prev []byte, value string) ([]byte, error) {
-	decodedLength := base64.URLEncoding.DecodedLen(len(prev))
-	decoded := make([]byte, decodedLength)
-	actualDecoded, err := base64.URLEncoding.Decode(decoded, prev)
-	if err != nil {
-		return nil, err
-	}
-	return decoded[:actualDecoded], nil
+        decodedLength := base64.URLEncoding.DecodedLen(len(prev))
+        decoded := make([]byte, decodedLength)
+        actualDecoded, err := base64.URLEncoding.Decode(decoded, prev)
+        if err == nil {
+                return decoded[:actualDecoded], nil
+        }
+        decodedLength := base64.RawURLEncoding.DecodedLen(len(prev))
+        decoded = make([]byte, decodedLength))
+        actualDecoded, err = base64.RawURLEncoding.Decode(decoded, prev)
+        if err != nil {
+                return nil, err
+        }
+        return decoded[:actualDecoded], nil
 }
 
 func transformPrepend(prev []byte, value string) ([]byte, error) {
